552be597da019473b0eff48514066590220860c6
ConnorLH <1206350636@qq.com>
2019-02-18 22:04:40 +0800

    框架的两方面认证（Client-APP和用户）

--- paascloud-common/paascloud-security-feign/src/main/java/com/paascloud/security/feign/OAuth2FeignAutoConfiguration.java
+++ paascloud-common/paascloud-security-feign/src/main/java/com/paascloud/security/feign/OAuth2FeignAutoConfiguration.java
@@ -32,10 +32,15 @@ import org.springframework.security.oauth2.common.AuthenticationScheme;
  * 如果是像QQ那样大公司，提供用户的Oauth2认证，那么需要使用@EnableOAuth2Client
  * 以此来隔离每个request，防止认证的信息被共享，每个请求都使用自己的-XX-
  * 我的理解是@EnableOAuth2Client 适用于与浏览器交互服务，这个服务中配置了受限url时，如果用户访问，那么这个功能将会自动引导用户走Oauth2流程。
- * 但是对于app交互的服务，这个功能是不适用的，因为app是没办法自己完成Oauth2流程的，必须用户参与（Browser->A->B->C，其中C资源受限这种情况同样会有问题）。所以在调用方确定被调用方的资源受限时同样也需要在自己的服务中配置调用处的逻辑是受限的，这样一直传递到浏览器交互服务，再使用@EnableOAuth2Client即可。
+ * 但是对于app交互的服务，这个功能是不适用的，因为app是没办法自己完成Oauth2流程的，必须用户参与（Browser->A->B->C，其中C资源受限这种情况同样会有问题）。所以在调用方确定被调用方的资源受限时同样也需要在自己的服务中配置调用处的逻辑是受限的，这样一直传递到浏览器交互服务下一级(B,为什么到B就行了，看下面)，再使用@EnableOAuth2Client即可。
  * 关于@EnableOAuth2Client这个注解包含了@EnableOAuth2Sso的功能，即发现要认证的时候帮我们去走Oauth2的授权流程（当然需要配置认证中心等信息）
  *
- *
+ *	这个框架的设计是每个客户端的url都必须要经过登录才能访问，这里的登录有两种情况，一个是客户端使用客户端模式在认证中心进行了认证，二是用户走流程到认证中心进行了认证。这两种情况认证中心都会给一个token。
+ *直接如何区分这两者，就需要每个资源服务的权限控制配置里面对需要用户登录的设置用户角色（比如所有用户都必须有user角色）。
+ * 所以这里只是做了客户端登录
+ * 什么时候用户登录？
+ * 请求到达网关时，网关（上面的A）配置了Oauth2的Client，它去访问某个资源服务时(上面的B)，发现要求认证，就会把用户302到认证中心去做认证，在认证完成之后将拿到的token与用户session绑订到一起，这样用户第二次发过来的请求就不必再认证了，因为session里面能找到token。这里注意，如果网关是集群部署，要做session共享才行。
+ * 总结上面，就是所有除了网关的服务都必须做权限配置（B调用C时，在知道C要求认证时这种情况B就必须配置）。
  * @author paascloud.net @gmail.com
  */
 @Configuration
@@ -81,6 +86,7 @@ public class OAuth2FeignAutoConfiguration {
 	@Bean("paascloudOAuth2RestTemplate")
 		public OAuth2RestTemplate oAuth2RestTemplate() {
 		// 这里使用默认提供的OAuth2ClientContext，用于将每个用户请求的认证信息隔离开。官方文档说如果我们不这么做，那么需要自己维护一个类似的数据结构用于隔离每个用户的请求。
+		// 这里第一个参数配置使用的4种oauth2认证模式哪一种
 		final OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(resourceDetails(), new DefaultOAuth2ClientContext());
 		// 请求构建工厂使用支持NIO的Netty4ClientHttpRequestFactory提高效率
 		// 默认是SimpleClientHttpRequestFactory这个，可能鸡肋
